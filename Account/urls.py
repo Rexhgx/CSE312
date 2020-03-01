c6c40a64d0e0bb2ae4bfa7ec5d557365344dcf80
Rex <rexhgx@gmail.com>
2020-03-01 12:31:56 -0500

    Update project

--- Account/urls.py
+++ Account/urls.py
@@ -3,11 +3,9 @@ from Account import views
 
 app_name = 'Account'
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
     path('sign_in/', views.sign_in, name='sign_in'),
-    path('welcome/<str:username>', views.welcome, name='welcome'),
     path('sign_up/', views.sign_up, name='sign_up'),
-    path('sign_up_going/', views.sign_up_going, name='sign_up_going'),
-    path('sign_up_success/', views.sign_up_success, name='sign_up_success'),
-    path('log_out/', views.log_out, name='log_out')
+    path('chat/', views.chat, name='chat'),
+    path('share/', views.share, name="share"),
+    path('share/', views.profile, name="profile"),
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,53 +1,45 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from . models import User
-from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_protect
+
+from .models import User
 
 
-def homepage(request):
-    return render(request, 'Account/homepage.html')
-
-
+@csrf_protect
 def sign_in(request):
-    try:
-        print(request )
-        user = User.objects.get(u_name=request.GET['username'], u_password=request.GET['password'])
-    except User.DoesNotExist:
-        error_message = "Sorry! You may enter the wrong username or wrong password"
-        return render(request, 'Account/homepage.html', {'error_message': error_message})
-    response = redirect(reverse('Account:welcome', args=(user.u_name,)))
-    return response
+    if request.method == "GET":
+        return render(request, "sign-in.html")
+    if request.method == "POST":
+        # try:
+        #     User.objects.get(u_name=request.GET['username'], u_password=request.GET['password'])
+        # except User.DoesNotExist:
+        #     error_message = "Sorry! You may enter the wrong username or wrong password"
+        #     return render(request, 'sign-in.html', {'error_message': error_message})
+        return redirect(reverse('Account:chat'))
 
 
+@csrf_protect
 def sign_up(request):
     if request.method == 'GET':
-        return render(request, 'Account/homepage.html')
+        return render(request, 'sign-up.html')
     if request.method == 'POST':
-        u_name = request.POST['username']
-        u_password = request.POST['password']
-        user = User.objects.filter(u_name=u_name).first()
-        if user:
-            return render(request, 'Account/signupgoing.html', {'error_message': "The user already exists."})
-        User.objects.create(u_name=u_name, u_password=u_password)
-        response = redirect(reverse('Account:sign_up_success'))
-        return response
+        # u_name = request.POST['username']
+        # u_password = request.POST['password']
+        # user = User.objects.filter(u_name=u_name).first()
+        # if user:
+        #     return render(request, 'Account/signupgoing.html', {'error_message': "The user already exists."})
+        # User.objects.create(u_name=u_name, u_password=u_password)
+        # response = redirect(reverse('Account:sign_up_success'))
+        return render(request, "sign-in.html")
 
 
-def sign_up_going(request):
-    return render(request, 'Account/signupgoing.html')
+def chat(request):
+    return render(request, "chat.html", context={"title": chat})
 
 
-def sign_up_success(request):
-    return render(request, 'Account/signupsuccess.html')
+def share(request):
+    return render(request, "share.html", context={"title": share})
 
 
-def welcome(request, username):
-    context = {
-        'username': username
-    }
-    return render(request, 'Account/user.html', context=context)
-
-
-def log_out(request):
-    return redirect(reverse('Account:homepage'))
+def profile(request):
+    return render(request, "profile.html", context={"title": profile})
