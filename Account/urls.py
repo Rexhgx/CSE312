e2948770f82e15e496d19a82f70063986679c366
Rex <rexhgx@gmail.com>
2020-04-17 17:52:58 -0400

    Add "MYSQL_PASSWORD" environment variable to docker

--- Account/urls.py
+++ Account/urls.py
@@ -3,6 +3,7 @@ from Account import views
 
 app_name = 'Account'
 urlpatterns = [
+    path('', views.redirect_to_sign_in),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('<user_name>/chat/', views.chat, name='<user_name>/chat'),
