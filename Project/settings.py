c43073b9b4d8cf831e4e3e7fc10cea51fb70b738
Rex <rexhgx@gmail.com>
2020-05-08 23:39:59 -0400

    Change database to mysql database

--- Project/settings.py
+++ Project/settings.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cse312',
-#         'USER': 'root',
-#         'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-#         'HOST': 'db',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cse312',
+        'USER': 'root',
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
